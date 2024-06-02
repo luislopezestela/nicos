e1c009bcca7f1b525284918778f76815de41e731
Luis lopez estela <jose_1.9.96@hotmail.com>
2024-06-02 06:19:21 -0500

    Update app_start.php

--- luisincludes/includes/app_start.php
+++ luisincludes/includes/app_start.php
@@ -488,7 +488,7 @@ if (!$wo['config']['can_use_chat']) {
 }
 
 $wo['config']['report_reasons'] = json_decode($wo['config']['report_reasons'],true);
-$wo['config']['filesVersion'] = "4.7.28";
+$wo['config']['filesVersion'] = "4.7.29";
 
 if ($wo['config']['filesVersion'] != $wo['config']['version']) {
     ini_set('display_errors', 0);
