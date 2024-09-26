ea2b5d2bf19569f050893061aa20d40efbb8228b
Luis lopez estela <jose_1.9.96@hotmail.com>
2024-09-26 16:13:15 -0500

    1.2 chat
    s

--- luisincludes/includes/app_start.php
+++ luisincludes/includes/app_start.php
@@ -534,7 +534,7 @@ if (!$wo['config']['can_use_chat']) {
 }
 
 $wo['config']['report_reasons'] = json_decode($wo['config']['report_reasons'],true);
-$wo['config']['filesVersion'] = "4.8.2";
+$wo['config']['filesVersion'] = "4.8.3";
 
 if ($wo['config']['filesVersion'] != $wo['config']['version']) {
     ini_set('display_errors', 0);
